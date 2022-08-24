e8eb3a7ba22880df600c92adb61a9399c90c26c1
Phil <philmore1979@gmail.com>
2022-08-23 20:52:35 -0400

    fine tune wrapper script

--- updater.sh
+++ updater.sh
@@ -3,6 +3,6 @@
 ### Wrapper script to call python script in
 ### the correct Virtual Environment
 # Switch to VirtualEnv
-source ./stubadge_env/bin/activate
+source ./stubadge_env/bin/activate stubadge_env
 # Call updater python script
 python3 stubadge_ldap_updater.py
