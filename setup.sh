57585d7c94fcd11feb2b310a1977f553e0184e97
root <root@rcit-stubadge.redclay.k12.de.us>
2022-08-24 00:07:00 +0000

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 ### Bash Script to configure python virtual env for Script ###
 # Switch to VirtualEnv
-source ./stubadge_env/bin/activate
+source ./stubadge_env/bin/activate stubadge_env
 # Install required packages
 # Self-created packages are included in the 
 # installer folder
