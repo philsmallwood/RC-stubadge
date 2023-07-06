226ef21356c1883066d462e2edf2b6807dcd5c56
Phil <phil.smallwood@live.com>
2023-07-06 08:32:52 -0400

    No longer needed

--- updater.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-### Main Script - Updater ###
-### Wrapper script to call python script in
-### the correct Virtual Environment
-# Switch to VirtualEnv
-source /updater/stubadge_env/bin/activate stubadge_env
-# Call updater python script
-python3 /updater/stubadge_ldap_updater.py
