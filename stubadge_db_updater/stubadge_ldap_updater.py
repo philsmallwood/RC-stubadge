76ad58df12fd54d17c752e9c3511963c065ced5d
Philip Smallwood <phil.smallwood@live.com>
2024-05-30 13:41:13 -0400

    reworked to grab data from ADDB

--- stubadge_db_updater/stubadge_ldap_updater.py
+++ stubadge_db_updater/stubadge_ldap_updater.py
@@ -6,14 +6,13 @@
 
 
 ###Import Modules###
-import ldap3 as ld
 import pandas as pd
 import time
 import datetime
 from os import getenv
 from dotenv import load_dotenv
+from app_logger import AppLogger
 from rc_smtp_send import google_smtp_send
-from rc_ad_info_export import ad_connection,ad_info_export
 from rc_google_py import write_log_to_google
 from sqlalchemy import create_engine 
 #######
@@ -36,76 +35,61 @@ log_file_name = 'StubadgeUpdaterLog-'
 alert_to_email = getenv('log_to_email')
 alert_subject = "Stubadge Updater - ERROR ALERT"
 smtp_pass = getenv('smtp_pass')
-# AD Variables
-dc_server = getenv('dc_server')
-bind_account = getenv('bind_account')
-bind_pass = getenv('bind_pass')
-search_base_list = getenv('search_base_list').split('$')
-search_base = tuple(search_base_list)
-search_scope = ld.SUBTREE
-ad_attributes = ['givenname','sn','distinguishedName',
-    'department','departmentNumber','description','title',
-    'UserPrincipalName','whenChanged','employeeID']
-search_filter = '(&(objectCategory=person)(objectClass=user)\
-    (!(userAccountControl:1.2.840.113556.1.4.803:=2)))' #Enabled Users
 # MySQL Variables
-mysql_user = getenv('mysql_user')
-mysql_pass = getenv('mysql_pass')
-mysql_server = getenv('mysql_server')
-mysql_db = getenv('mysql_db')
-mysql_connection_url = f"mysql+pymysql://{mysql_user}:{mysql_pass}@{mysql_server}/{mysql_db}"
+adinfo_mysql_username = getenv('adinfo_mysql_username')
+adinfo_mysql_pass = getenv('adinfo_mysql_pass')
+adinfo_mysql_hostname = getenv('adinfo_mysql_hostname')
+adinfo_mydb_name = getenv('adinfo_mydb_name')
+student_table_name = 'Students'
+stubadge_mysql_user = getenv('stubadge_mysql_user')
+stubadge_mysql_server = getenv('stubadge_mysql_server')
+stubadge_mysql_pass = getenv('stubadge_mysql_pass')
+stubadge_mysql_db = getenv('stubadge_mysql_db')
+stubadge_student_table_name = 'ldapData'
 ########
 
 
-###Log Begin###
-log_file += "------------------\n"
-log_file += f"The Student Badge App Updater Script was started on {start_time} \n"
-log_file += "---\n"
+### Start Logger ###
+logger = AppLogger('Stubadge Student LDAP Info Updater App', start_time)
 ###########
 
-###Get Info from AD###
-#Connect
+### Get Info from AD MySQL DB ###
+# Connect to Database
+engine = create_engine(
+    f'mysql+pymysql://{adinfo_mysql_username}:{adinfo_mysql_pass}@{adinfo_mysql_hostname}/{adinfo_mydb_name}?charset=utf8mb4')
+# Read Student Info to DF
 try:
-    dc_connection = ad_connection(dc_server,bind_account,bind_pass)
-    #Pull AD Data
-    df_student_info = ad_info_export(dc_connection,\
-        search_base,\
-        ad_attributes,\
-        search_scope,\
-        search_filter)
-    #Log Info Download
-    log_file += "---\n"
-    log_file += "Student Info Successfully Downloaded from AD\n"
-    log_file += "---\n"
+    df_student_info = pd.read_sql(student_table_name, con=engine)
+    logger.log_success()
 except:
-    #Log Error
-    log_file += "---\n"
-    log_file += "!! Error !! Student Info Downloaded Failed!\n"
-    log_file += "---\n"
+    logger.log_error()
+# Disconnect from Database
+engine.dispose()
 ###########
+
+### Format DF ###
+df_student_info = df_student_info[['givenName','sn','distinguishedName',
+                        'department','departmentNumber','description','title',
+                        'userPrincipalName','whenChanged','employeeID']].copy()
 
 ###Update MySQL Table###
 try:
-    mysqlEngine = create_engine(mysql_connection_url, echo=True)
-    mysqlConnection = mysqlEngine.connect()
-    df_student_info.to_sql('ldapData',\
-        con = mysqlEngine,\
+    engine = create_engine(f"mysql+pymysql://{stubadge_mysql_user}:{stubadge_mysql_pass}@{stubadge_mysql_server}/{stubadge_mysql_db}"
+, echo=True)
+    df_student_info.to_sql(stubadge_student_table_name,
+        con = engine,\
         if_exists = 'replace',\
         index=False)
     #Log Database Update
-    log_file += "---\n"
-    log_file += "Student Data Successfully Updated\n"
-    log_file += "---\n"
+    logger.log_success()
 except:
-    #Log Error
-    log_file += "---\n"
-    log_file += "!! Error !! Student Data Update Failed!\n"
-    log_file += "---\n"
+    logger.log_error()
 ########
 
 
 ###Write Log to Google###
 try: 
+    log_file = logger.get_log()
     write_log_to_google(google_auth_key, \
         network_team_drive_id, \
         stubadge_log_folder_id, \
